d88aceb167532b08bb5b79684bb8e8beff9dc8f6
handong.liu <handong.liu@host.docker.internal>
2019-04-26 10:53:11 +0800

    add BusinessException

---
+++ sonybatchcommon/src/main/java/com/soni/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.soni.exception;
+
+public class BusinessException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8431529808855261193L;
+	
+	public BusinessException(String message) {
+		
+	}
+
+}
