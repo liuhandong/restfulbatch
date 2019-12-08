50b7410f51589dd91374c2d2402c743e9de9aa45
liuhandong <liuhandong11@sina.com>
2019-12-09 04:31:39 +0800

    add

---
+++ sonybatchjob/src/main/java/com/app/it/java/example/CreateHighOrderFunction.java
@@ -0,0 +1,37 @@
+package com.app.it.java.example;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class CreateHighOrderFunction {
+
+    private static List list;
+    private static List result = new ArrayList();
+
+    public CreateHighOrderFunction(List list) {
+        CreateHighOrderFunction.list = list;
+    }
+
+    //find Even Number is a function which takes a Predicate
+
+    public static List findEvenNumbers(Predicate predicateToApply) {
+
+        BiFunction<List, Predicate, List> filter;
+        //here filter is a BiFunction Which act as a high order Function it takes a list,another function predicate
+        // as a argument
+
+        filter = (listOfIntegers, predicate) -> {
+            //result.addAll(list.stream().filter(predicate).collect(Collectors.toList()));
+            return result;
+        };
+        //called the apply method of BiFunction and sent list and predicateToApply as argument
+
+        filter.apply(list, predicateToApply);
+        return result;
+
+    }
+}
